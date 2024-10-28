49fc4934d421452c3d5f82a7363fb24135d2be27
Tiolulope <juliusdedeji01@gmail.com>
2024-10-28 22:43:00 +0100

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SpecialOffers from "./SpecialOffers";
+
+export {
+  Hero,
+  SuperQuality,
+  PopularProducts,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  Footer,
+  SpecialOffers,
+};
